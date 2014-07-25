5fed6f581b3ebf4d90330b8fb1b022f89095338c
Mike Piccolo <mfpiccolo@gmail.com>
2014-07-25 00:33:19 -0700

    Refactor searcher and add math operators for number columns

--- app/services/Searcher.rb
+++ app/services/Searcher.rb
@@ -1,6 +1,8 @@
 class Searcher
 
-  attr_reader :user, :type, :query_string, :query_array, :scope, :sql_string
+  Operators = ["<=", ">=", "<", ">", "=", "(", ")"]
+
+  attr_reader :user, :type, :query_string, :query_array, :scope, :sql_string, :sql_array
 
   def self.call(user, type, query_string)
     new(user, type, query_string).call
@@ -11,56 +13,60 @@ class Searcher
     @type = type
     @query_string = query_string
     @sql_string = ""
+    @sql_array = []
   end
 
   def call
-    if ["&&", "||", ":"].any? { |join| query_string.include? join }
-      build_query_array
-      scope = @user.models.where(otype: type)
-      build_sql_string(query_array)
-      scope.where(sql_string)
-    else
-      @user.models.where(otype: type).search_data(query_string)
-    end
+    build_sql_array(query_string)
+    build_sql_string
+    scope = @user.models.where(otype: type)
+    scope.where(@final_sql_array.join(""))
   end
 
   private
 
-  def build_query_array
-    @query_array = query_string.split("||").map do |queries|
-      if queries.include?("(") && queries.include?(")")
-        a = []
-        queries.scan(/\(([^\)]+)\)/).flatten.first.split("&&").each do |q_string|
-          pair_array = q_string.split(":")
-          a << Hash[pair_array[0].strip, pair_array[1].strip]
-        end
-        a
-      elsif queries.include?("&&")
-        a = []
-        queries.split("&&").each do |q_string|
-          pair_array = q_string.split(":")
-          a << Hash[pair_array[0].strip, pair_array[1].strip]
-        end
-        a
-      else
-        pair_array = queries.split(":")
-        Hash[pair_array[0].strip, pair_array[1].strip]
-      end
+  def build_sql(array)
+    array.each do |string|
+      build_sql_array(string)
     end
   end
 
-  def build_sql_string(array, join = " OR ")
-    array.each_with_index do |h,i|
-      if h.is_a? Array
-        build_sql_string(h, " AND ")
-      else
-        query = "(((to_tsvector('simple', coalesce(data ->> '#{h.keys.first}', ''))) @@ (to_tsquery('simple', ''' ' || '#{h.values.first}' || ' '''))))"
+  def build_sql_array(string, parens: false)
+    ordered_ops_array = string.to_enum(:scan, /(\(.+\)|\|{2}|&&|[^|&]*)/)
+      .map { Regexp.last_match }.map(&:to_s).delete_if(&:empty?)
 
-        if i == 0
-          sql_string << query
-        else
-          sql_string << join + query
-        end
+    if parens
+      @count = ordered_ops_array.size
+    end
+
+    if string.size == ordered_ops_array.first.size && string.include?("(") && string.include?(")")
+      sql_array << "("
+      build_sql_array(string[1..-2], parens: true)
+    elsif ordered_ops_array.length == 1
+      sql_array << string
+      if @count.present?
+        @count -= 1
+        sql_array << ")" if @count == 0
+      end
+    else
+      build_sql ordered_ops_array
+    end
+  end
+
+  def build_sql_string
+    @final_sql_array = sql_array.map(&:strip).map do |string|
+      if string.include?(":")
+        q = string.split(":")
+        "(((to_tsvector('simple', coalesce(data ->> '#{q[0].strip}', ''))) @@ (to_tsquery('simple', ''' ' || '#{q[1].strip}' || ' '''))))"
+      elsif [">=", "<=", ">", "<"].any? { |join| string.include? join }
+        q = string.gsub(/\s+/m, ' ').strip.split(" ")
+        "(data->>'#{q[0].strip}')::int #{q[1]} #{q[2]}"
+      elsif string == "||"
+        " OR "
+      elsif string == "&&"
+        " AND "
+      else
+        string
       end
     end
   end
